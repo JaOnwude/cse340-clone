802f78af9934982c15bf0d113d6bc5e1afa5b7cd
JaOnwude <ojamesuchenna61@gmail.com>
2025-09-25 07:02:29 +0100

    Updated session secret

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
-/* ***********************
+/* ************************
  * Middleware
  * ************************/
  app.use(session({
